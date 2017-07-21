b3d3ba845e5a8b345a21406d153deacfe8503330
Fritz <fre.healy@gmail.com>
2017-07-21 15:11:43 -0400

    major templating home

--- template-parts/content-index.php
+++ template-parts/content-index.php
@@ -7,13 +7,63 @@
  * @package ACStarter
  */
 
+$background_image = get_field("background_image");
 ?>
 
-<article id="post-<?php the_ID(); ?>" <?php post_class("template-index"); ?>>
-	<header>
-		<h1><?php the_title(); ?></h1>
-	</header>
-	<div class="copy">
-		<?php the_content();?>
-	</div><!--.copy-->
+<article id="post-<?php the_ID(); ?>" <?php post_class("template-index"); ?> <?php 
+	if($background_image):
+		echo 'style="background-image: url('.$background_image['url'].');"';
+	endif;
+?>>
+	<?php $title = get_field("title");
+	$sub_heading = get_field("sub_heading");
+	$copy = get_field("copy");
+	$learn_more_link = get_field("learn_more_link");
+	$learn_more_text = get_field("learn_more_text");
+	$bottom_buttons = get_field("bottom_buttons");?>
+	<?php if($background_image):?>
+		<img class="mobile" src="<?php echo $background_image['sizes']['medium'];?>" alt="<?php echo $background_image['alt'];?>">
+	<?php endif;?>
+	<div class="row-1">
+		<div class="col-1">
+			<?php if($title):?>
+				<header>
+					<h1><?php echo $title;?></h1>
+				</header>
+			<?php endif;?>
+			<?php if($sub_heading):?>
+				<div class="sub-heading">
+					<?php $sub_heading;?>
+				</div><!--.sub-heading-->
+			<?php endif;?>
+			<?php if($copy):?>
+				<div class="copy">
+					<?php echo $copy;?>
+				</div><!--.copy-->
+			<?php endif;?>
+			<?php if($learn_more_link&&$learn_more_text):?>
+				<div class="learn-more">
+					<a href="<?php echo $learn_more_link;?>">
+						<?php echo $learn_more_text;?>
+					</a>
+				</div><!--.learn-more-->
+			<?php endif;?>
+		</div><!--.col-1-->
+		<div class="col-2">
+			<!--.search-->
+		</div><!--.col-2-->
+	</div><!--.row-1-->
+	<?php if($bottom_buttons):?>
+		<div class="row-2">
+			<?php foreach($bottom_buttons as $row):
+				if($row['button_link']&&$row['button_text']):?>
+					<div class="button">
+						<a href="<?php echo $row['button_link'];?>">
+							<?php echo $row['button_text'];?>
+						</a>
+					</div><!--.button-->
+				<?php endif;
+			endforeach;?>
+		</div><!--.row-2-->
+	<?php endif;?>
 </article><!-- #post-## -->
