75f62e9bfbe7c97597c44757d909c223f611eb4f
Fritz <fre.healy@gmail.com>
2017-09-22 10:54:31 -0400

    adding login and login redirect

--- inc/theme.php
+++ inc/theme.php
@@ -143,3 +143,185 @@ function ac_first_and_last_menu_class($items) {
   return $items;
 }
 add_filter('wp_nav_menu_objects', 'ac_first_and_last_menu_class');
+/**
+ * Redirect user after successful login.
+ *
+ * @param string $redirect_to URL to redirect to.
+ * @param string $request URL the user is coming from.
+ * @param object $user Logged user's data.
+ * @return string
+ */
+ function my_login_redirect( $redirect_to, $request, $user ) {
+	//is there a user to check?
+	if ( isset( $user->roles ) && is_array( $user->roles ) ) {
+		$login = get_the_permalink(451);
+		if ( in_array( 'customer', $user->roles ) && $login && strcmp($request,$login)===0 ) {
+			$link = get_the_permalink( 413);//get order form
+			if ( $link ) {
+				return $link;
+			} else {
+				return $redirect_to;
+			}
+		} elseif(in_array( 'administrator', $user->roles )){
+			return $redirect_to;
+		} elseif (in_array( 'customer', $user->roles )) {
+      $link = get_the_permalink( 413);//get order form
+			if ( $link ) {
+				return $link;
+			} else {
+				return $redirect_to;
+			}
+		} else {
+			return $redirect_to;
+		}
+	} else {
+		return $redirect_to;
+	}
+}
+function shefa_wp_login_form( $args = array() ) {
+		$defaults = array(
+			'echo'           => true,
+			// Default 'redirect' value takes the user back to the request URI.
+			'redirect'       => ( is_ssl() ? 'https://' : 'http://' ) . $_SERVER['HTTP_HOST'] . $_SERVER['REQUEST_URI'],
+			'form_id'        => 'loginform',
+			'label_username' => __( 'Username or Email' ),
+			'label_password' => __( 'Password' ),
+			'label_remember' => __( 'Remember Me' ),
+			'label_log_in'   => __( 'Log In' ),
+			'id_username'    => 'user_login',
+			'id_password'    => 'user_pass',
+			'id_remember'    => 'rememberme',
+			'id_submit'      => 'wp-submit',
+			'remember'       => true,
+			'value_username' => '',
+			// Set 'value_remember' to true to default the "Remember me" checkbox to checked.
+			'value_remember' => false,
+		);
+
+		/**
+		 * Filter the default login form output arguments.
+		 *
+		 * @since 3.0.0
+		 *
+		 * @see wp_login_form()
+		 *
+		 * @param array $defaults An array of default login form arguments.
+		 */
+		$args = wp_parse_args( $args, apply_filters( 'login_form_defaults', $defaults ) );
+
+		/**
+		 * Filter content to display at the top of the login form.
+		 *
+		 * The filter evaluates just following the opening form tag element.
+		 *
+		 * @since 3.0.0
+		 *
+		 * @param string $content Content to display. Default empty.
+		 * @param array $args Array of login form arguments.
+		 */
+		$login_form_top = apply_filters( 'login_form_top', '', $args );
+
+		/**
+		 * Filter content to display in the middle of the login form.
+		 *
+		 * The filter evaluates just following the location where the 'login-password'
+		 * field is displayed.
+		 *
+		 * @since 3.0.0
+		 *
+		 * @param string $content Content to display. Default empty.
+		 * @param array $args Array of login form arguments.
+		 */
+		$login_form_middle = apply_filters( 'login_form_middle', '', $args );
+
+		/**
+		 * Filter content to display at the bottom of the login form.
+		 *
+		 * The filter evaluates just preceding the closing form tag element.
+		 *
+		 * @since 3.0.0
+		 *
+		 * @param string $content Content to display. Default empty.
+		 * @param array $args Array of login form arguments.
+		 */
+		$login_form_bottom = apply_filters( 'login_form_bottom', '', $args );
+
+		$form     = '
+               <form name="' . $args['form_id'] . '" id="' . $args['form_id'] . '" action="' . esc_url( site_url( 'wp-login.php', 'login_post' ) ) . '" method="post">
+	                        ' . $login_form_top . '
+	                        <p class="login-username">
+	                                <label for="' . esc_attr( $args['id_username'] ) . '">' . esc_html( $args['label_username'] ) . '</label>
+	                                <input type="text" name="log" id="' . esc_attr( $args['id_username'] ) . '" class="input" value="' . esc_attr( $args['value_username'] ) . '" size="20" />
+	                        </p>
+	                        <p class="login-password">
+	                                <label for="' . esc_attr( $args['id_password'] ) . '">' . esc_html( $args['label_password'] ) . '</label>
+	                                <input type="password" name="pwd" id="' . esc_attr( $args['id_password'] ) . '" class="input" value="" size="20" />
+	                        </p>
+	                        ' . $login_form_middle;
+							$form_end = '' . ( $args['remember'] ? '<p class="login-remember"><label><input name="rememberme" type="checkbox" id="' . esc_attr( $args['id_remember'] ) . '" value="forever"' . ( $args['value_remember'] ? ' checked="checked"' : '' ) . ' /> ' . esc_html( $args['label_remember'] ) . '</label></p>' : '' ) . '
+	                        <p class="login-submit">
+	                                <input type="submit" name="wp-submit" id="' . esc_attr( $args['id_submit'] ) . '" class="button-primary" value="' . esc_attr( $args['label_log_in'] ) . '" />
+	                                <input type="hidden" name="redirect_to" value="' . esc_url( $args['redirect'] ) . '" />
+	                        </p>
+	                        ' . $login_form_bottom.'
+							</form>';
+
+		if ( $args['echo'] ) {
+			$wp_error = new WP_Error();
+			global $error;
+			/**
+			 * Filters the message to display above the login form.
+			 *
+			 * @since 2.1.0
+			 *
+			 * @param string $message Login message text.
+			 */
+			$message = apply_filters( 'login_message', '' );
+			if ( !empty( $message ) )
+				echo $message . "\n";
+
+			// In case a plugin uses $error rather than the $wp_errors object
+			if ( !empty( $error ) ) {
+				$wp_error->add('error', $error);
+				unset($error);
+			}
+
+			if ( $wp_error->get_error_code() ) {
+				$errors = '';
+				$messages = '';
+				foreach ( $wp_error->get_error_codes() as $code ) {
+					$severity = $wp_error->get_error_data( $code );
+					foreach ( $wp_error->get_error_messages( $code ) as $error_message ) {
+						if ( 'message' == $severity )
+							$messages .= '	' . $error_message . "<br />\n";
+						else
+							$errors .= '	' . $error_message . "<br />\n";
+					}
+				}
+				if ( ! empty( $errors ) ) {
+					/**
+					 * Filters the error messages displayed above the login form.
+					 *
+					 * @since 2.1.0
+					 *
+					 * @param string $errors Login error message.
+					 */
+					echo '<div id="login_error">' . apply_filters( 'login_errors', $errors ) . "</div>\n";
+				}
+				if ( ! empty( $messages ) ) {
+					/**
+					 * Filters instructional messages displayed above the login form.
+					 *
+					 * @since 2.5.0
+					 *
+					 * @param string $messages Login messages.
+					 */
+					echo '<p class="message">' . apply_filters( 'login_messages', $messages ) . "</p>\n";
+				}
+			}
+			echo $form;
+			do_action( 'login_form' );
+			echo $form_end;
+		} else
+	                return $form.$form_end;
+}
---
+++ template-parts/content-login.php
@@ -0,0 +1,41 @@
+<?php
+/**
+ * Template part for displaying page content in page.php.
+ *
+ * @link https://codex.wordpress.org/Template_Hierarchy
+ *
+ * @package ACStarter
+ */
+
+?>
+
+<article id="post-<?php the_ID(); ?>" <?php post_class("template-login"); ?>>
+	<section class="row-1 main-row">
+		<div class="row-1">
+			<div class="col-1">
+				<header>
+					<h1><?php the_title();?></h1>
+					<div class="spacer"></div>
+				</header>
+			</div><!--.col-1-->
+			<aside class="col-2">
+				<?php get_template_part('template-parts/search',"form");?>
+			</aside><!--.col-2-->
+		</div><!--.row-1-->
+		<div class="row-2">
+			<div class="col-1">
+				<div class="copy">
+					<?php the_content();?>
+				</div><!--.copy-->
+				<?php shefa_wp_login_form();?>
+				<?php $registration_title = get_field("registration_title");
+				if($registration_title):?>
+					<header>
+						<h1><?php echo $registration_title;?></h1>
+					</header>
+				<?php endif;?>
+				<?php echo do_shortcode('[gravityform id="1" title="false" description="false"]');?>
+			</div><!--.col-1-->
+		</div><!--.row-2-->
+	</section><!--.row-1-->
+</article><!-- #post-## -->
